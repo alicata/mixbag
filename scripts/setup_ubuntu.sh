36d3fcb603295699fcbd97cf7bcdb09f39ed6214
Aaron Licata <licataa@zoho.com>
2015-11-18 22:56:43 -0800

    initial script to setup ubuntu for common development tasks

---
+++ scripts/setup_ubuntu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get -y install git cmake make build-essential vim pkg-config
+sudo apt-get update
+sudo apt-get -y install v4l-utils v4l2ucp cheese
+sudo apt-get update
+sudo apt-get -y install libgtk2.0-dev libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get -y install libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get update
+sudo apt-get -y install libssl-dev python-dev python-numpy
+sudo apt-get -y install python-pip
+sudo apt-get -y install mesa-utils
+
+echo 'PYTHON DEPENDENCIES'
+sudo pip install virtualenv
+sudo pip install numpy matplotlib scipy
+
+
+
