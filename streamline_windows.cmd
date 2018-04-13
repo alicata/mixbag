d76b2adf0f63be0488f056d84e2ed80535ed67bd
alicata <alicata@users.noreply.github.com>
2018-04-13 13:25:17 -0700

    Update streamline_windows.cmd

--- streamline_windows.cmd
+++ streamline_windows.cmd
@@ -6,3 +6,24 @@ git clone https://github.com/Sycnex/Windows10Debloater.git
 
 cd Windows10Debloater
 powershell -executionpolicy bypass -file Windows10Debloater.ps1
+
+
+echo "---------------------"
+echo "stop windows updates "
+echo "---------------------"
+stop-service wuauserv
+set-service wuauserv -startuptype disabled
+
+
+echo "---------------------"
+echo "power settings for continous operation "
+echo "---------------------"
+powercfg.exe -change -monitor-timeout-ac 0
+powercfg.exe -change -monitor-timeout-dc 0
+powercfg.exe -change -disk-timeout-ac 0
+powercfg.exe -change -disk-timeout-dc 0
+powercfg.exe -change -standby-timeout-ac 0
+powercfg.exe -change -standby-timeout-dc 0
+powercfg.exe -change -hibernate-timeout-ac 0
+powercfg.exe -change -hibernate-timeout-dc 0
+
