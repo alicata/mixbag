a5482c652b7adf5adf431de1b29e4b513edd14b4
alicata <alicata@users.noreply.github.com>
2018-04-13 09:36:00 -0700

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,10 @@
+choco install -y vim
+choco install -y git
+git config --system --unset credential.helper
+
+choco install -y miniconda3 /AddToPath:1 /RegisterPython:1 /D=C:\Users\lattepanda\Miniconda3
+echo "add to path, "
+echo "c:\tools\miniconda3"
+echo "c:\tools\miniconda3\scripts"
+
+choco install -y openssh
